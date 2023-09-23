a80e270fe7c2f84df46e22f39a32ed43e631692a
Anique <aniquekhan004@gmail.com>
2023-09-23 13:56:35 +0500

    added contact form in views

--- fuzail_tempora/fuzail_app/urls.py
+++ fuzail_tempora/fuzail_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from fuzail_app.views import index
+from fuzail_app.views import index,contact_view
 
 urlpatterns = [
-    path("",index,name="index")
+    path("",index,name="index"),
+    path("contact_view",contact_view,name="contact_view")
 ]
--- fuzail_tempora/fuzail_app/views.py
+++ fuzail_tempora/fuzail_app/views.py
@@ -1,5 +1,44 @@
 from django.shortcuts import render
+from fuzail_app.forms import ContactForm
+from fuzail_app.models import Contact
+from django.core.mail import send_mail
+from django.conf import settings
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    form = ContactForm
+    context = {
+        "form":form
+    }
+    return render(request,"index.html",context)
+
+
+def contact_view(request):
+    if request.method == "POST" and request.is_ajax():
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            first_name = form.cleaned_data.get("first_name")
+            last_name = form.cleaned_data.get("last_name")
+            message = form.cleaned_data.get("message")
+            subject = form.cleaned_data.get("subject")
+
+
+            Contact.objects.create(email=email,subject=subject, first_name=first_name,last_name=last_name, message=message)
+
+            # Sending email to myself
+            # email_subject = "You got a message from your live portfolio website"
+            # email_msg = f"Name : {name}\nEmail : {email}\nMessage : {message}"
+            # send_mail(
+            #     subject=email_subject,
+            #     message=email_msg,
+            #     from_email=settings.EMAIL_HOST_USER,
+            #     recipient_list=[settings.EMAIL_HOST_USER]
+            # )
+
+            return JsonResponse({"success": True})
+        else:
+            return JsonResponse({"errors": form.errors}, status=400)
+    else:
+        return JsonResponse({"error": "Invalid request method"}, status=405)
